f961bbc35f7ec9950730dc77f3813c762b7a3c0b
Adam Zygadlewicz <adam@zygadlewicz.com>
2021-10-15 00:45:56 +0200

    Update [id].js

--- pages/c/[id].js
+++ pages/c/[id].js
@@ -1,103 +1,40 @@
 import React from 'react'
 import {
-    connect,
-    configure,
-    reduxWrapper,
-    PartnerPromo,
-    PartnerPerformance,
-    Wrapper,
-    Markdown,
-    MyButton as Button
-  } from 'eventjuicer-site-components';
-  
+connect,
+configure,
+reduxWrapper,
+Wrapper,
+MyButton as Button
+} from 'eventjuicer-site-components';
 
-  import settings from '../../settings';
 
-  import {
-    AssignmentInd as Badges,
-    Mic as Presentation,
-    Videocam as Video_interview,
-    Web as Brand_highlight,
-    MonetizationOn as Earlybird,
-    Event as Meetups,
-    Description as Blog,
-    VolumeUp as Rollups
-} from '@material-ui/icons'
+import settings from '../../settings';
+
+
+const PageCompany = ({id}) => {
+
+return (
+<Wrapper>
+</Wrapper>
+);
+
+}
 
 
 
-  const PageCompany = ({id}) => {
-    
-  return (
-    <Wrapper>
 
-     <PartnerPromo id={id} icons={{
-      Badges, 
-      Presentation,
-      Video_interview,
-      Brand_highlight,
-      Earlybird,
-      Meetups,
-      Blog,
-      Rollups
-    }}
-    sidebar={
-      <div>
-      <PartnerPerformance icons={{
-        Badges, 
-        Presentation,
-        Video_interview,
-        Brand_highlight,
-        Earlybird,
-        Meetups,
-        Blog,
-        Rollups
-      }} 
-      limit={5}
-      />
-      <Button href="/" label="exhibitor.ranking.button" />
-      </div>
-    }
-    />
+export const getServerSideProps = reduxWrapper.getServerSideProps(async (props) => {
 
-    <Markdown label="exhibitor.promo.about" />
+  const {params:{id}} = props;
 
-    </Wrapper>
-  );
-  
-  }
-
-  export async function getStaticPaths() {
-
-    return {
-      paths: [],
-      fallback: "blocking" 
-    };
-     
-  }
-
-  
-  export const getStaticProps = reduxWrapper.getStaticProps(async (props) => {
-  
-    const {params:{id}} = props;
-
-    await configure(props, {
-      settings : settings,
-      preload : ['ranking', 'prizes']
-    })
-
-    return {
-        props : {
-            id
-        },
-        revalidate : 10
-    }
-  
+  await configure(props, {
+  settings : settings,
+  preload : ['ranking', 'prizes']
   })
-  
+
+  return { props : {id}, revalidate : 10}
+
+})
 
 
-
-  
-  export default connect()(PageCompany);
-  
+export default connect()(PageCompany);
