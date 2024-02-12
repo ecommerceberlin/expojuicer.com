408c433f60ef030c23582d53c6c0e9b2c1ca7ff5
Adam Zygadlewicz <adam@zygadlewicz.com>
2024-02-12 23:35:25 +0100

    settings updated for EBE8 / resolves https://github.com/ecommerceberlin/exhibitors/issues/194

--- settings/index.js
+++ settings/index.js
@@ -7,9 +7,9 @@ const settings =  {
             'admin': '1751bfe48d5ad21fd9d'
         },
         lang_api_endpoint : 'https://api.eventjuicer.com/proxy?url=https%3A%2F%2Flocalise.biz%2Fapi%2Fexport%2Fall.json%3Fformat%3Dmulti%26pretty%26key%3DSHiwxgKaPMx_KThQH2zcdzwiKEMzuNBm',    
-        api : "https://api.eventjuicer.com/v1/public/hosts/targiehandlu.pl",
+        api : "https://api.eventjuicer.com/v1/public/hosts/ecommerceberlin.com",
         // proxy: "https://api.eventjuicer.workers.dev?url=",
-        post_api : "https://api.eventjuicer.com/v1/public/hosts/targiehandlu.pl/register",
+        post_api : "https://api.eventjuicer.com/v1/public/hosts/ecommerceberlin.com/register",
         service_api : "https://api.eventjuicer.com/v1/services",
         og_image : "",
     
